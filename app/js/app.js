e7b0b9a5dda32749fe04d2a384d429aff81402cc
Evg-Ko <31292566+9420128@users.noreply.github.com>
2020-12-17 14:23:33 +0400

    start

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
+// import UIkit from 'uikit'
+// import Icons from 'uikit/dist/js/uikit-icons'
+// UIkit.use(Icons)
+
+import 'jsFiles/header'
+
+
+
+
+
+
+
+
